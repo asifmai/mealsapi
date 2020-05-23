da5b69f6c5ab8a7a71bc22fb273b72cf9263da7b
iqbal <asifmai@hotmail.com>
2020-05-24 01:27:03 +0500

    update

--- routes/api.js
+++ routes/api.js
@@ -10,13 +10,13 @@ router.post('/login', async (req, res, next) => {
   try {
     // Check if email field exists in body
     if (!req.body.email) {
-      return res.status(200).send('Please send a user email with the request...');
+      return res.status(400).send('Please send a user email with the request...');
     }
 
     // check if email is in allowed users list
     const email = req.body.email.toLowerCase();
     if (!allowedUsers.some(au => au.toLowerCase() == email.toLowerCase())) {
-      return res.status(200).send('The email is not allowed...');
+      return res.status(400).send('The email is not allowed...');
     }
 
     // Check if user is registered or not
@@ -29,7 +29,7 @@ router.post('/login', async (req, res, next) => {
       newUser = true;
     }
 
-    // Return jsonwebtoken (So user is logged in as he registers)
+    // Return jsonwebtoken
     const payload = {
       user: {
         id: foundUser.id
@@ -39,7 +39,7 @@ router.post('/login', async (req, res, next) => {
     jwt.sign(
       payload,
       process.env.JWT_SECRET,
-      { expiresIn: parseInt(process.env.JWT_EXPIRE) },      // Expires in an hour
+      { expiresIn: parseInt(process.env.JWT_EXPIRE) },
       (err, token) => {
         if (err) throw err;
         return res.status(200).json({
@@ -55,43 +55,67 @@ router.post('/login', async (req, res, next) => {
 })
 
 const registerUser = async (email) => {
-  const newUser = new User({email });
-  await newUser.save();
+  try {
+    const newUser = new User({email });
+    await newUser.save();
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
 }
 
 router.get('/me', authMW, async (req, res, next) => {
-  const user = await User.findOne({email: req.user.email}).populate('profile').exec();
-  res.status(200).json(user);
+  try {
+    const user = await User.findOne({email: req.user.email}).populate('profile').exec();
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
 })
 
 router.post('/profile', authMW, async (req, res, next) => {
-  console.log(req.body);
-  const {
-    name,
-    height,
-    weight,
-    age,
-    activityLevel,
-    noOfMeals,
-    calories,
-    carbs,
-    fats,
-    proteins
-  } = req.body;
-  const newProfile = new Profile({
-    name, height, weight, age, activityLevel, noOfMeals, calories, carbs, fats, proteins
-  })
-  await newProfile.save();
-  await User.findOneAndUpdate(req.user.id, {profile: newProfile.id});
-  res.status(200).send('Profile added...');
+  try {
+    const {
+      name,
+      height,
+      weight,
+      age,
+      activityLevel,
+      noOfMeals,
+      calories,
+      carbs,
+      fats,
+      proteins
+    } = req.body;
+    const newProfile = new Profile({
+      name, height, weight, age, activityLevel, noOfMeals, calories, carbs, fats, proteins
+    })
+    await newProfile.save();
+    await User.findOneAndUpdate(req.user.id, {profile: newProfile.id});
+    res.status(200).send('Profile added...');
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
 });
 
 router.get('/meals', authMW, async (req, res, next) => {
-  res.status(200).send('Meals get route')
+  try {
+    res.status(200).send('Meals get route');
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
 })
 
 router.post('/meals', authMW, async (req, res, next) => {
-  res.status(200).send('Meals post route')
+  try {
+    res.status(200).send('Meals post route');
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
 })
 
 module.exports = router;
