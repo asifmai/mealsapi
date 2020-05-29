56af51c7fae37a31c4d7c29a785a607b5879472b
iqbal <asifmai@hotmail.com>
2020-05-29 15:53:03 +0500

    update

--- helpers/allowedusers.js
+++ helpers/allowedusers.js
@@ -1,5 +1,12 @@
 module.exports = [
   'asifmai@hotmail.com',
   'vjhameed3@gmail.com',
-  'asifmai2k@gmail.com'
+  'asifmai2k@gmail.com',
+  'iqbalmai@outlook.com',
+  'testuser@gmail.com',
+  'anotheruser@hotmail.com',
+  'testaccount@gmail.com',
+  'testuser2@gmail.com',
+  'testuser3@gmail.com',
+  'testuser4@gmail.com'
 ]
